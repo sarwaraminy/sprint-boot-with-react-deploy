922b87fac6c00b706870f0134ad4bb2a692d007c
Sarwar <sarwaraminy@gmail.com>
2024-04-24 15:31:33 +0430

    Added the excel output logic, you can get the report as excel or you can insert the data from table to Excel

--- src/main/java/com/example/demo/controller/RoomControllerView.java
+++ src/main/java/com/example/demo/controller/RoomControllerView.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +19,12 @@ import com.example.demo.data.Room;
 import com.example.demo.services.RoomService;
 
 import org.springframework.ui.Model;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+
 
 @Controller
 @RequestMapping(path="/rooms/view")
@@ -87,5 +99,48 @@ public class RoomControllerView {
 			}
 			return "redirect:/rooms/view";
 		}
+
+		// create a method that handle the output to MS Excel
+		@GetMapping("/excel")
+		public ResponseEntity<byte[]> createExcelOutput() throws IOException {
+			List<Room> roomList = roomService.getAllRooms();
+
+			//create Excel workbook
+			Workbook workbook = new XSSFWorkbook();
+			Sheet sheet = workbook.createSheet("Room Data");
+
+			// Create titles
+			Row titles = sheet.createRow(0);
+			titles.createCell(0).setCellValue("ID");
+			titles.createCell(1).setCellValue("Name");
+			titles.createCell(2).setCellValue("Room Number");
+			titles.createCell(3).setCellValue("Bed Info");
+
+			//Create data file
+			int rowNum = 1;
+			for(Room room: roomList){
+				Row row = sheet.createRow(rowNum++);
+				row.createCell(0).setCellValue(room.getId());
+				row.createCell(1).setCellValue(room.getName());
+				row.createCell(2).setCellValue(room.getRoomNumber());
+				row.createCell(3).setCellValue(room.getBedInfo());
+			}
+
+			//convert workbook to byte array
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			workbook.write(outputStream);
+			byte[] excelBytes = outputStream.toByteArray();
+
+			// Set Ecell content type and headers
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+            headers.setContentDispositionFormData("attachment", "room_data.xlsx");
+
+			return new ResponseEntity<>(excelBytes, headers, HttpStatus.OK);
+		}
+		
+		//Create a method to handle PDF version
+		//@GetMapping("/pdf")
+		
 	
 }
